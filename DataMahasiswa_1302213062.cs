beefcd9917add8b263dbe9b3bc6f849c30cd84a4
PC9-IFLAB <127019755+muthiakhrnsa3@users.noreply.github.com>
2023-04-03 08:25:37 +0700

    first commit

---
+++ DataMahasiswa_1302213062.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace modul7_kelompok_2
+{
+    internal class DataMahasiswa_1302213062
+    {
+        public void ReadJSON()
+        {
+            string jsonText = File.ReadAllText("E:/modul7_kelompok_2/jurnal7_1_1302213062.json");
+            dynamic jsonObj = JsonConvert.DeserializeObject(jsonText);
+
+            string namadepan = jsonObj.firstName;
+            string namabelakang = jsonObj.lastName;
+            string gender = jsonObj.gender;
+            string age = jsonObj.age;
+            string streetAdr = jsonObj.address.streetAddress;
+            string cityAdr = jsonObj.address.city;
+            string stateAdr = jsonObj.address.state;
+
+
+            Console.WriteLine("Nama " + namadepan + " " + namabelakang + " " + gender + " " + age + " " + streetAdr + " " + cityAdr + " " + stateAdr);
+            Console.WriteLine("Mata kuliah yang diambil: ");
+            for (int i = 0; i < jsonObj.courses.Count; i++)
+            {
+                Console.WriteLine($"MK {i + 1} {jsonObj.courses[i].code} - {jsonObj.courses[i].name}");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul7_kelompok_2;
+
+class main
+{
+    public static void Main(String[] args)
+    {
+        DataMahasiswa_1302213062 Dt = new DataMahasiswa_1302213062();
+        Dt.ReadJSON();
+    }
+}
