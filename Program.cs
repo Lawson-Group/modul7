4575668e65e98d2c945aee646b93ffe31bb9b1f1
PC7-IFLAB <novitasabila70@gmail.com>
2023-04-03 07:50:25 +0700

    menambahkan class TeamMembers_1302213030

--- Program.cs
+++ Program.cs
@@ -4,3 +4,10 @@ using modul7_kelompok_2;
 
 DataMahasiswa_1302213030 dm = new DataMahasiswa_1302213030();
 dm.ReadJSON();
+
+Console.WriteLine();
+Console.WriteLine("========================================================");
+Console.WriteLine();
+
+TeamMembers_1302213030 tm = new TeamMembers_1302213030();
+tm.ReadJSON();
---
+++ TeamMembers_1302213030.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace modul7_kelompok_2
+{
+    internal class TeamMembers_1302213030
+    {
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText(@"E:\modul7\jurnal7_2_1302213030.json");
+
+            dynamic jsonObject = JsonConvert.DeserializeObject(jsonString);
+
+            Console.WriteLine("Team member list: ");
+            for (int i = 0; i < jsonObject.members.Count; i++)
+            {
+                Console.WriteLine($"{jsonObject.members[i].nim} {jsonObject.members[i].firstName} {jsonObject.members[i].lastName}" +
+                    $" {jsonObject.members[i].age} {jsonObject.members[i].gender} ");
+            }
+        }
+    }
+}
